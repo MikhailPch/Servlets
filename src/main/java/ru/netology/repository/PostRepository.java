6a90012bc45b4ab47d8f9454068045f533c87db8
MikhailPachurin <120920635+MikhailPch@users.noreply.github.com>
2023-11-21 22:06:38 +0500

    Initial commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,43 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+
+  private final Map<Long, Post> posts = new ConcurrentHashMap<>(){};
+  private final AtomicLong counter = new AtomicLong(0L);
+
+  public List<Post> all() {
+
+    return new ArrayList<>(posts.values());
+  }
+
+  public Optional<Post> getById(long id) {
+
+    return Optional.ofNullable(posts.get(id));
+  }
+
+  public Post save(Post post) {
+    if (post.getId() != 0 && !posts.containsKey(post.getId())) {
+      throw new NotFoundException();
+    }
+    if (post.getId() == 0) {
+      var newId = counter.incrementAndGet();
+      post.setId(newId);
+    }
+    posts.put(post.getId(), post);
+    return post;
+  }
+
+  public void removeById(long id) {
+    if (!posts.containsKey(id))
+      throw new NotFoundException();
+    posts.remove(id);
+  }
+}
