e360d90d5bf0bf1d40539882d36aab44e4f52c8f
MikhailPachurin <120920635+MikhailPch@users.noreply.github.com>
2023-11-26 16:48:58 +0500

    Annotation DI

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -5,7 +5,7 @@ import ru.netology.controller.PostController;
 import ru.netology.model.Post;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
-
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,9 +21,8 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext("ru.netology");
+        controller = context.getBean(PostController.class);
     }
 
     @Override
